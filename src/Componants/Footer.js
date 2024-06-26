9b1aa7b62e26eb246ce4e1bd347d490d1f66437b
Rohit Manohar <manoharrohit372@gmail.com>
2024-06-26 19:30:07 +0530

    Done

--- src/Componants/Footer.js
+++ src/Componants/Footer.js
@@ -17,13 +17,13 @@ const Footer = ({ darkMode }) => {
   });
 
   return (
-    <footer className={` ${darkMode ? 'bg-white text-white' : 'bg-black text-gray-800'} py-4`}>
+    <footer className={` ${darkMode ? 'bg-black text-white' : 'bg-white text-gray-800'} py-4`}>
       <div className="container mx-auto px-6 lg:px-4 flex flex-col items-center justify-center font-serif">
         <p className="text-sm">&copy; {new Date().getFullYear()} NewScope. All rights reserved.</p>
 
         <div className="flex flex-col items-center justify-center mt-6">
           <button
-            className="text-2xl font-serif bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 rounded-lg mb-4 transition duration-300 font-lato transform hover:scale-105 cursor-pointer"
+            className={`text-2xl font-serif ${darkMode ? 'bg-gray-800 hover:bg-gray-600' : 'bg-gray-600 hover:bg-gray-500'} text-white py-2 px-4 rounded-lg mb-4 transition duration-300 font-lato transform hover:scale-105 cursor-pointer`}
             onClick={toggleForm}
           >
             Let's Connect 🚀🚀
@@ -34,10 +34,10 @@ const Footer = ({ darkMode }) => {
             className="w-full max-w-md"
           >
             {showForm && (
-              <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4 font-serif">
+              <form className={`bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4 ${darkMode ? 'text-white' : 'text-gray-800'} font-serif`}>
                 <div className="mb-4">
                   <label
-                    className="block text-white text-sm font-bold mb-2"
+                    className="block text-sm font-bold mb-2 text-white"
                     htmlFor="name"
                   >
                     Name
@@ -51,7 +51,7 @@ const Footer = ({ darkMode }) => {
                 </div>
                 <div className="mb-6">
                   <label
-                    className="block text-white text-sm font-bold mb-2"
+                    className="block text-sm font-bold mb-2 text-white"
                     htmlFor="email"
                   >
                     Email
@@ -65,7 +65,7 @@ const Footer = ({ darkMode }) => {
                 </div>
                 <div className="mb-4">
                   <label
-                    className="block text-white text-sm font-bold mb-2"
+                    className="block text-sm font-bold mb-2 text-white"
                     htmlFor="message"
                   >
                     Message
@@ -79,7 +79,7 @@ const Footer = ({ darkMode }) => {
                 </div>
                 <div className="flex items-center justify-center">
                   <button
-                    className="bg-gray-700 hover:bg-gray-600 text-white font-bold font-serif py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-transform transform hover:scale-105 cursor-pointer"
+                    className={`bg-gray-700 hover:bg-gray-600 text-white font-bold font-serif py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-transform transform hover:scale-105 cursor-pointer ${darkMode ? '' : 'text-gray-800'}`}
                     type="button"
                   >
                     Submit
@@ -92,14 +92,14 @@ const Footer = ({ darkMode }) => {
       </div>
 
       {/* Social Media Icons */}
-      <div className="flex justify-end  mt-4 mr-6">
-        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-800 mx-2">
+      <div className="flex justify-end mt-4 mr-6">
+        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className={`text-gray-60== hover:text-gray-800 mx-2 ${darkMode ? 'text-white' : 'text-gray-600'}`}>
           <FaInstagram className="w-6 h-6" />
         </a>
-        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-800 mx-2">
+        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className={`text-gray-600 hover:text-gray-800 mx-2 ${darkMode ? 'text-white' : 'text-gray-600'}`}>
           <FaFacebook className="w-6 h-6" />
         </a>
-        <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-800 mx-2">
+        <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className={`text-gray-600 hover:text-gray-800 mx-2 ${darkMode ? 'text-white' : 'text-gray-600'}`}>
           <FaTwitter className="w-6 h-6" />
         </a>
       </div>
