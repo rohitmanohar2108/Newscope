103bf1e0804a96fb68ab6142efd39fcde3d86434
Rohit Manohar <manoharrohit372@gmail.com>
2024-07-04 11:43:47 +0530

    done

--- src/Componants/Browse.js
+++ src/Componants/Browse.js
@@ -16,7 +16,7 @@ const Browse = () => {
     const fetchArticles = async () => {
       try {
         const result = await axios.get(
-          'https://newsapi.org/v2/everything?q=geopolitics&apiKey=ea8156fa512e49ddae5f4360b29947f0'
+          'https://newsapi.org/v2/everything?q=india&apiKey=ea8156fa512e49ddae5f4360b29947f0'
         );
         setArticles(result.data.articles);
         setLoading(false); // Set loading to false after data is fetched
